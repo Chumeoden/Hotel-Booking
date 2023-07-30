8da470ba435bb0246d10b47fe50d6021501b3004
Chumeoden <117746775+Chumeoden@users.noreply.github.com>
2023-07-30 18:59:35 +0700

    Update all

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', function () {
+Route::get('/admin', function () {
     return view('auth.login');
+});
+Route::get('/', function () {
+    return view('home');
 });
 
 Route::group(['middleware' => ['isAdmin','auth'],'prefix' => 'admin', 'as' => 'admin.'], function() {
